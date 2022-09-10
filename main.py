e717107467ab0a2abecd96e8d6444f58b163efab
yhao-z <59129069+yhao-z@users.noreply.github.com>
2022-09-10 17:08:35 +0800

    add reqs

--- main.py
+++ main.py
@@ -68,7 +68,6 @@ if __name__ == "__main__":
             t0 = time.time()
             with tf.GradientTape() as tape:
                 k0, label = sample
-                csm = None
 
                 if k0 is None:
                     continue
